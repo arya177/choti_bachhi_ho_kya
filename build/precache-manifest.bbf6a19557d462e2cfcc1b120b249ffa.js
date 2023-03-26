e8d2464b7092a7cc97eb2a740eb8e42ff44a528d
Arya Shahi <aryashahi2002@gmail.com>
2023-03-26 05:35:19 +0530

    Firebase added

---
+++ build/precache-manifest.bbf6a19557d462e2cfcc1b120b249ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c9d19d3682697579f3d",
+    "url": "/static/css/main.afde68f1.chunk.css"
+  },
+  {
+    "revision": "3c9d19d3682697579f3d",
+    "url": "/static/js/main.708d8ba5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a0f723cd56726b02d17",
+    "url": "/static/js/2.862de3c1.chunk.js"
+  },
+  {
+    "revision": "a856430e8ba6c93db5cea0cf017dbdba",
+    "url": "/static/media/Default_Image.a856430e.jpg"
+  },
+  {
+    "revision": "16b75b4515df4d20ba98274fd762a335",
+    "url": "/index.html"
+  }
+];
--- src/example.jsx
+++ src/example.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useCallback } from "react";
-
+import Button from '@mui/material/Button';
 import DropZone from "./DropZone";
 import TrashDropZone from "./TrashDropZone";
 import SideBarItem from "./SideBarItem";
 import Row from "./Row";
 import initialData from "./initial-data";
+import { collection, addDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore"; 
+import {db} from '../src/firebase';
 import {
   handleMoveWithinParent,
   handleMoveToDifferentParent,
@@ -15,6 +18,9 @@ import { Paper } from "@mui/material";
 
 import { SIDEBAR_ITEMS, SIDEBAR_ITEM, COMPONENT, COLUMN } from "./constants";
 import shortid from "shortid";
+
+let final_layout=[];
+let final_component={};
 
 const Container = () => {
   const initialLayout = initialData.layout;
@@ -28,6 +34,7 @@ const Container = () => {
       setLayout(handleRemoveItemFromLayout(layout, splitItemPath));
     },
     [layout]
+    
   );
 
   const handleDrop = useCallback(
@@ -120,15 +127,68 @@ const Container = () => {
     );
   };
 
-  // dont use index for key when mapping over items
-  // causes this issue - https://github.com/react-dnd/react-dnd/issues/342
+  const handleSave = async () => {
+
+      // await addData(final_component);
+      console.log("component", final_component);
+      // let map = new Map()
+      let obj = {}
+      Object.keys(components).map(item => {
+        let tempObj = {
+          id : components[item].id,
+          type : components[item].type
+        };
+        obj = {item: tempObj}
+  })
+     
+      await addData(obj)
+      await addData(layout)
+      
+  }
+  const addData = async (e) => {
+    console.log(e)
+    // let temp={};
+    // e.values.map(item=>{
+    //   let id = item.id;
+    //   let type = item.type;
+    //   temp={id,type}
+
+    // })
+
+   
+    try {
+        console.log(e);
+        if(e===layout){
+          // db.collection("layout").doc("hacktank").set(layout)
+        // const docRef = await addDoc(collection(db, "layout"), {
+        //   layout: layout,    
+        // })
+        // console.log("Document written with ID: ", docRef.id);
+        await setDoc(doc(db, "layout", "hacktank"), {layout});
+
+        }
+        else {
+          await setDoc(doc(db, "component", "hacktank"), e);
+          // const docRef = await addDoc(collection(db, "component"), {
+          //   components:e 
+          // });
+          // console.log("Document written with ID: ", docRef.id);
+        }
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+  }
   return (
     <div className="body">
+      <div className="sidePane">
       <div className="sideBar">
         {Object.values(SIDEBAR_ITEMS).map((sideBarItem, index) => (
           <SideBarItem key={sideBarItem.id} data={sideBarItem} />
         ))}
       </div>
+      <Button variant="contained" className="btn" onClick={handleSave}>Save</Button>
+      </div>
+
       <div className="pageContainer">
         <Paper elevation={4} style={{
           width: "95%",
